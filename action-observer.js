5ba0f2e6a8053a6b650b37933d59819afeede7b1
Ryan Morr <rm.morr@gmail.com>
2015-03-17 19:05:23 -0400

    Update source code

--- action-observer.js
+++ action-observer.js
@@ -1,35 +1,92 @@
 (function(win){ 
     'use strict';
     
-    // Shortcut for document element    
-    var docElement = win.document.documentElement, 
-    // Object literal that maps elements to callback functions via the data-observe attribute value
-    listeners = {}, 
-    // Shortcut for hasOwnProperty
-    has = listeners.hasOwnProperty,
-    // ActionObserver is disabled by default
-    disabled = true;
+    var ActionObserver = {}, 
+    docElement = win.document.documentElement, 
+    has = {}.hasOwnProperty,
+    listeners = {};
+
+    /**
+     * Map a callback function to the element in 
+     * which you would like to observe action events on
+     * 
+     * @param {String} key
+     * @param {Function} fn
+     * @param {Object} ctx
+     */
+    ActionObserver.bind = function(key, fn, ctx){
+        listeners[key] = {
+            triggered: false,
+            fn: fn.bind(ctx || win)
+        };
+    };
+
+    /**
+     * Remove a callback function bound to an 
+     * element being observed
+     * 
+     * @param {String} key
+     */
+    ActionObserver.unbind = function(key){
+        if(has.call(listeners, key)){
+            delete listeners[key];
+        }
+    };
+
+    /**
+     * Was an action event triggered on the element 
+     * corresponding to the provided key
+     * 
+     * @param {String} key
+     * @return {Boolean}
+     */
+    ActionObserver.isTriggered = function(key){
+        return has.call(listeners, key) && listeners[key].triggered;
+    };
     
-    // Handle action events (click, submit) on the document element 
+    /**
+     * Disable ActionObserver functionality and remove the 
+     * event listeners from the document element
+     */
+    ActionObserver.disable = function(){
+        docElement.removeEventListener('click', onEvent, false);
+        docElement.removeEventListener('submit', onEvent, false);
+    };
+    
+    // Handle action events (click, submit) on the document
     function onEvent(e){
-        var el = find(e.target), key;
+        var el = find(e.target), key, exists, fn;
         if(el){
-            // If the element is a form and it is not a submit event, return
+            // If the element is a form and it is not a submit 
+            // event, return
             if(el.nodeName.toLowerCase() === 'form' && e.type !== 'submit'){
                 return;
             }
-            // Get the value of the data-observe attribute used to bind the callback function
+            // Get the value of the data-observe attribute used 
+            // to find the callback function
             key = el.getAttribute('data-observe');
-            // Check that a callback function exists with the key
-            if(has.call(listeners, key)){
-                // Invoke the callback function passing the element and event object
-                listeners[key](e, el);
+            exists = has.call(listeners, key);
+            if(!exists){
+                // Mark the element as triggered
+                listeners[key] = {
+                    tiggered: true
+                };
+            }
+            fn = listeners[key].fn;
+            // If a callback exists, invoke the function passing 
+            // the element and event object
+            if(fn){
+                fn(e, el);
             }
         }
     }
     
-    // Climb up the DOM tree to find the element containing the data-observe attribute based on an event's target
+    // Climb up the DOM tree to find the element containing the 
+    // data-observe attribute based on an event's target
     function find(el){
+        if('closest' in el){
+            return el.closest('[data-observe]');
+        }
         while(el && el !== docElement){
             if(el.hasAttribute('data-observe')){
                 return el;  
@@ -38,51 +95,13 @@
         }
         return null;
     }
+
+    // Listen for click and submit events on the document 
+    // when they bubble up
+    docElement.addEventListener('click', onEvent, false);
+    docElement.addEventListener('submit', onEvent, false);
+
+    // Expose `ActionObserver`
+    win.ActionObserver = ActionObserver;
     
-    // Define ActionObserver globally   
-    win.ActionObserver = {
-        
-        /**
-         * Map a callback function to the element in which you would like to observe action events on
-         * @param  {String}   key Value of the data-observe attribute for the element that is being observed
-         * @param  {Function} fn  The callback function that is to be invoked when the element dispatches an action event
-         * @param  {Object}   ctx The context to invoke the callback function in
-         */
-        bind: function(key, fn, ctx){
-            listeners[key] = fn.bind(ctx || win);
-        },
-
-        /**
-         * Remove a callback function bound to an element being observed
-         * @param  {String} key Value of the data-observe attribute for the element that is being observed
-         */
-        unbind: function(key){
-            if(has.call(listeners, key)){
-                delete listeners[key];
-            }
-        },
-        
-        /**
-         * Enable ActionObserver functionality and add the event listeners to the document element (disabled by default)
-         */
-        enable: function(){
-            if(disabled){
-                disabled = false;
-                docElement.addEventListener('click', onEvent, false);
-                docElement.addEventListener('submit', onEvent, false);
-            }
-        },
-        
-        /**
-         * Disable ActionObserver functionality and remove the event listeners from the document element
-         */
-        disable: function(){
-            if(!disabled){
-                disabled = true;
-                docElement.removeEventListener('click', onEvent, false);
-                docElement.removeEventListener('submit', onEvent, false);
-            }
-        }
-    };
-
 })(this);
