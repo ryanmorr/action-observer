3033b60a06c7aa985e59738743fe20680f95d3fe
Ryan Morr <rm.morr@gmail.com>
2015-03-17 21:07:27 -0400

    Add unit tests

---
+++ test/test-action-observer.js
@@ -0,0 +1,68 @@
+describe('ActionObserver', function(){
+    'use strict';
+
+    var expect = chai.expect;
+
+    function triggerEvent(el, type){
+        var event = document.createEvent('Event');
+        event.initEvent(type, true, true);
+        el.dispatchEvent(event);
+    }
+
+    it('should capture click events', function(){
+        var spy = sinon.spy(function(e){
+            e.preventDefault();
+        });
+        ActionObserver.bind('link', spy);
+        var link = document.getElementById('link');
+        triggerEvent(link, 'click');
+        var spyCall = spy.getCall(0);
+        expect(spy.calledOnce).to.equal(true);
+        expect(spyCall.args[0] instanceof Event).to.equal(true);
+        expect(spyCall.args[0].type).to.equal('click');
+        expect(spyCall.args[1]).to.equal(link);
+        expect(ActionObserver.isTriggered('link')).to.equal(true);
+    });
+
+    it('should capture submit events', function(){
+        var spy = sinon.spy(function(e){
+            e.preventDefault();
+        });
+        ActionObserver.bind('form', spy);
+        var form = document.getElementById('form');
+        triggerEvent(form, 'submit');
+        var spyCall = spy.getCall(0);
+        expect(spy.calledOnce).to.equal(true);
+        expect(spyCall.args[0] instanceof Event).to.equal(true);
+        expect(spyCall.args[0].type).to.equal('submit');
+        expect(spyCall.args[1]).to.equal(form);
+        expect(ActionObserver.isTriggered('form')).to.equal(true);
+    });
+
+    it('should allow callbacks to be removed', function(){
+        var spy = sinon.spy(function(e){
+            e.preventDefault();
+        });
+        ActionObserver.bind('link', spy);
+        var link = document.getElementById('link');
+        triggerEvent(link, 'click');
+        expect(spy.calledOnce).to.equal(true);
+        ActionObserver.unbind('link', spy);
+        triggerEvent(link, 'click');
+        expect(spy.calledOnce).to.equal(true);
+    });
+
+    it('should allow to be disabled', function(){
+        var spy = sinon.spy(function(e){
+            e.preventDefault();
+        });
+        ActionObserver.bind('link', spy);
+        var link = document.getElementById('link');
+        triggerEvent(link, 'click');
+        expect(spy.calledOnce).to.equal(true);
+        ActionObserver.disable();
+        triggerEvent(link, 'click');
+        expect(spy.calledOnce).to.equal(true);
+    });
+
+});
