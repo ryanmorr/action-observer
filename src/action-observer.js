bae2e36b38698d4fb1c2d684e24a7d55f5e52246
Ryan Morr <rm.morr@gmail.com>
2023-03-30 12:59:26 -0400

    Add support for storing and retrieving all actions

--- src/action-observer.js
+++ src/action-observer.js
@@ -3,28 +3,43 @@ const docElement = window.document.documentElement;
 docElement.addEventListener('click', onEvent, false);
 docElement.addEventListener('submit', onEvent, false);
 
-function onEvent(evt) {
-    const el = evt.target.closest('[action-observe]');
-    if (el) {
-        if (el.nodeName.toLowerCase() === 'form' && evt.type !== 'submit') {
+function onEvent(event) {
+    const element = event.target.closest('[action-observe]');
+    if (element) {
+        if (element.nodeName.toLowerCase() === 'form' && event.type !== 'submit') {
             return;
         }
-        const key = el.getAttribute('action-observe');
+        const key = element.getAttribute('action-observe');
         let listener = listeners.get(key);
         if (!listener) {
-            listener = {};
+            listener = {
+                actions: []
+            };
             listeners.set(key, listener);
         }
-        listener.triggered = true;
+        listener.actions.push({
+            element,
+            event
+        });
         const callback = listener.callback;
         if (callback) {
-            callback.call(el, evt, el);
+            callback(event, element);
         }
     }
 }
 
 export function observe(key, callback) {
-    listeners.set(key, {triggered: false, callback});
+    let listener = listeners.get(key);
+    if (!listener) {
+        listener = {callback, actions: []};
+        listeners.set(key, listener);
+    } else {
+        listener.callback = callback;
+    }
+    const actions = listener.actions;
+    if (actions.length > 0) {
+        actions.forEach((action) => callback(action.event, action.element));
+    }
 }
 
 export function unobserve(key) {
@@ -33,9 +48,15 @@ export function unobserve(key) {
     }
 }
 
-export function wasTriggered(key) {
-    const listener = listeners.get(key);
-    return listener && listener.triggered;
+export function getActions(key) {
+    let listener = listeners.get(key);
+    if (!listener) {
+        listener = {
+            actions: []
+        };
+        listeners.set(key, listener);
+    }
+    return listener.actions;
 }
 
 export function disable() {
--- test/specs/action-observer.js
+++ test/specs/action-observer.js
@@ -1,4 +1,4 @@
-import { observe, unobserve, wasTriggered, disable } from '../../src/action-observer';
+import { observe, unobserve, getActions, disable } from '../../src/action-observer';
 
 describe('action-observer', () => {
     const container = document.createElement('div');
@@ -12,7 +12,6 @@ describe('action-observer', () => {
 
     it('should capture click events', () => {
         const spy = sinon.spy((e) => e.preventDefault());
-
         observe('link', spy);
 
         const event = new MouseEvent('click', {bubbles: true});
@@ -21,14 +20,12 @@ describe('action-observer', () => {
         expect(spy.callCount).to.equal(1);
         expect(spy.args[0][0]).to.equal(event);
         expect(spy.args[0][1]).to.equal(link);
-        expect(wasTriggered('link')).to.equal(true);
 
         unobserve('link');
     });
 
     it('should capture submit events', () => {
         const spy = sinon.spy((e) => e.preventDefault());
-
         observe('form', spy);
 
         const event = new Event('submit', {bubbles: true});
@@ -37,14 +34,86 @@ describe('action-observer', () => {
         expect(spy.callCount).to.equal(1);
         expect(spy.args[0][0]).to.equal(event);
         expect(spy.args[0][1]).to.equal(form);
-        expect(wasTriggered('form')).to.equal(true);
 
         unobserve('form');
     });
 
+    it('should call an observe callback for a previously dispatched click event', () => {        
+        const event = new MouseEvent('click', {bubbles: true});
+        link.dispatchEvent(event);
+
+        const spy = sinon.spy((e) => e.preventDefault());
+        observe('link', spy);
+    
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0][0]).to.equal(event);
+        expect(spy.args[0][1]).to.equal(link);
+    
+        unobserve('link');
+    });
+
+    it('should call an observe callback for a previously dispatched submit event', () => {
+        const event = new Event('submit', {bubbles: true});
+        form.dispatchEvent(event);
+
+        const spy = sinon.spy((e) => e.preventDefault());
+        observe('form', spy);
+
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0][0]).to.equal(event);
+        expect(spy.args[0][1]).to.equal(form);
+
+        unobserve('form');
+    });
+
+    it('should call an observe callback for previous and future events', () => {        
+        const event1 = new MouseEvent('click', {bubbles: true});
+        link.dispatchEvent(event1);
+
+        const spy = sinon.spy((e) => e.preventDefault());
+        observe('link', spy);
+    
+        expect(spy.callCount).to.equal(1);
+        expect(spy.args[0][0]).to.equal(event1);
+        expect(spy.args[0][1]).to.equal(link);
+
+        const event2 = new MouseEvent('click', {bubbles: true});
+        link.dispatchEvent(event2);
+
+        expect(spy.callCount).to.equal(2);
+        expect(spy.args[1][0]).to.equal(event2);
+        expect(spy.args[1][1]).to.equal(link);
+    
+        unobserve('link');
+    });
+
+    it('should return all captured actions', () => {
+        let actions = getActions('link');
+        expect(actions.length).to.equal(0);
+
+        const event1 = new MouseEvent('click', {bubbles: true});
+        link.dispatchEvent(event1);
+        expect(actions.length).to.equal(1);
+        expect(actions[0].event).to.equal(event1);
+        expect(actions[0].element).to.equal(link);
+
+        const event2 = new MouseEvent('click', {bubbles: true});
+        link.dispatchEvent(event2);
+        expect(actions.length).to.equal(2);
+        expect(actions[1].event).to.equal(event2);
+        expect(actions[1].element).to.equal(link);
+
+        const event3 = new MouseEvent('click', {bubbles: true});
+        link.dispatchEvent(event3);
+        expect(actions.length).to.equal(3);
+        expect(actions[2].event).to.equal(event3);
+        expect(actions[2].element).to.equal(link);
+
+        unobserve('link');
+    });
+
     it('should support callbacks being removed', () => {
         const spy = sinon.spy((e) => e.preventDefault());
-
         observe('link', spy);
 
         link.dispatchEvent(new MouseEvent('click', {bubbles: true}));
@@ -60,7 +129,6 @@ describe('action-observer', () => {
 
     it('should support functionality being disabled', () => {
         const spy = sinon.spy();
-
         observe('link', spy);
 
         disable();
